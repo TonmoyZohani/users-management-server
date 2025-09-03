563c322581bc9312cd55f00ba8b700337b7b2010
TonmoyZohani <tonmoyzohani@gmail.com>
2025-09-03 22:51:21 +0600

    server running fixed

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get((req, res) => {
+// app.get((req, res) => {
+//   res.send(`Users management server is running`);
+// });
+
+app.get("/", (req, res) => {
   res.send(`Users management server is running`);
 });
 
