7b2332428a240038b3dbb05bfea56272017f28cd
TonmoyZohani <tonmoyzohani@gmail.com>
2025-09-05 19:12:11 +0600

    post api created

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.get("/users", (req, res) => {
   res.send(users);
 });
 
+app.post("/users", (req, res) => {
+  console.log(`Post Api is being hit`);
+  console.log(req.body);
+});
+
 app.listen(port, () => {
   console.log(`Server is running port ${port}`);
 });
