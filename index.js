faa2d106e4b57423f464a820c02ccbb01a73d79e
TonmoyZohani <tonmoyzohani@gmail.com>
2025-09-05 20:22:26 +0600

    connected to the mongo db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,6 +13,32 @@ const users = [
   { id: 2, name: "Bob Smith", email: "bob.smith@example.com" },
   { id: 3, name: "Charlie Brown", email: "charlie.brown@example.com" },
 ];
+
+
+const uri = "mongodb+srv://zohani0804:asem0804@cluster0.d9m08dj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
   res.send(`Users management server is running`);
