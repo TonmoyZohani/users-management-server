56ce5be162324ce64dff0bfa7b48b764557ba654
TonmoyZohani <tonmoyzohani@gmail.com>
2025-09-06 19:44:26 +0600

    delete function operation done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -46,9 +46,13 @@ async function run() {
     });
 
     // POST: delete user
-    app.delete("/users/:id", (req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       console.log(`Please delete from database id ${id}`);
+
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Ping the DB
